1217a8ff8f7e78d3843ec0b93b3a2795da2ff046
Maqsud Samatov <maqsudsamadov719@gmail.com>
2026-02-06 12:44:20 +0500

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -16,7 +16,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="font-sans antialiased">
-<div class="flex h-screen bg-gray-100">
+<div style="background: yellow;" class="flex h-screen bg-gray-100">
     <!-- Sidebar -->
     <x-admin-sidebar />
 

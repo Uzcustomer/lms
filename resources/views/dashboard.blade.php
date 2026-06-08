48133f5f5a055d01b38ff024d53b0c2d22f24a94
Maqsud Samatov <maqsudsamadov719@gmail.com>
2026-06-08 10:23:13 +0500

    Remove dashboard welcome title

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,7 @@
 <x-app-layout>
-    @php
-        $dashboardTitle = 'Assalom aleykum Toshkent davlat tibbiyot universisteti Termiz filiali LMS foydalanuvchilari';
-    @endphp
-
     <x-slot name="header">
         <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
-            {{ $dashboardTitle }}
+            {{ __('Dashboard') }}
         </h2>
     </x-slot>
     @if ($errors->any())
@@ -42,12 +38,7 @@
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <h1 class="mb-4 text-2xl font-bold text-gray-900 dark:text-gray-100">
-                        {{ $dashboardTitle }}
-                    </h1>
-                    <p>
-                        {{ __("Tizimga kirish muvafaqiyatli amalga oshirildi!") }}
-                    </p>
+                    {{ __("Tizimga kirish muvafaqiyatli amalga oshirildi!") }}
                 </div>
             </div>
         </div>

bfbe35cf07c642fdc058576ce1f927a86c96103c
Maqsud Samatov <maqsudsamadov719@gmail.com>
2026-07-24 15:20:09 +0500

    Revert timetable role access changes

--- routes/web.php
+++ routes/web.php
@@ -864,10 +864,7 @@ Route::prefix('admin')->name('admin.')->group(function () {
 
         // Dars jadvali tuzish (aSc Timetables uslubida)
         Route::prefix('dars-jadvali-tuzish')->name('timetable.')
-            ->middleware([
-                \Spatie\Permission\Middleware\RoleMiddleware::class . ':superadmin|admin|kichik_admin|registrator_ofisi|oquv_bolimi|oquv_bolimi_boshligi',
-                \App\Http\Middleware\TimetableAssignmentAccess::class,
-            ])
+            ->middleware(\Spatie\Permission\Middleware\RoleMiddleware::class . ':superadmin|admin|kichik_admin|registrator_ofisi')
             ->group(function () {
                 Route::get('/', [\App\Http\Controllers\Admin\TimetableController::class, 'index'])->name('index');
                 Route::post('/boards', [\App\Http\Controllers\Admin\TimetableController::class, 'storeBoard'])->name('boards.store');

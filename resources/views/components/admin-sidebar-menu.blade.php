380877e9806dec84755c060455cf0c9c9a3d289e
Maqsud Samatov <maqsudsamadov719@gmail.com>
2026-07-24 15:03:06 +0500

    Show timetable creation link for academic roles

--- resources/views/components/admin-sidebar-menu.blade.php
+++ resources/views/components/admin-sidebar-menu.blade.php
@@ -830,7 +830,7 @@
         </a>
         @endif
 
-        @if($hasActiveRole(['superadmin', 'admin', 'kichik_admin', 'registrator_ofisi']))
+        @if($hasActiveRole(['superadmin', 'admin', 'kichik_admin', 'registrator_ofisi', 'oquv_bolimi', 'oquv_bolimi_boshligi']))
         <a href="{{ route('admin.oquv-reja.index') }}"
            class="sidebar-link {{ request()->routeIs('admin.oquv-reja.*') ? 'sidebar-active' : '' }}">
             <svg class="w-5 h-5 mr-3 sidebar-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">

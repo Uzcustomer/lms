17d842e5c80c4a5f9dbac8d06fd2e2146a1817a0
Maqsud Samatov <maqsudsamadov719@gmail.com>
2026-07-28 11:06:49 +0500

    Allow faculty scoped timetable grid settings

--- app/Models/TimetableGridSetting.php
+++ app/Models/TimetableGridSetting.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class TimetableGridSetting extends Model
 {
     protected $fillable = [
-        'board_id', 'specialty_name', 'course', 'days', 'pairs_per_day', 'weeks',
+        'board_id', 'faculty_name', 'specialty_name', 'course', 'days', 'pairs_per_day', 'weeks',
     ];
 }

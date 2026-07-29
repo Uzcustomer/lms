b99547e2c36bfc5dbe12632a40f09ce08557a00b
Maqsud Samatov <maqsudsamadov719@gmail.com>
2026-07-29 15:03:45 +0500

    Add auditorium teacher assignment model

---
+++ app/Models/AuditoriumTeacher.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AuditoriumTeacher extends Model
+{
+    protected $table = 'auditorium_teacher';
+
+    protected $fillable = [
+        'board_id',
+        'auditorium_id',
+        'teacher_id',
+        'is_general',
+    ];
+
+    protected $casts = [
+        'is_general' => 'boolean',
+    ];
+}

c1aace8c8c3703fee2b7a913f5d36c304209f258
Maqsud Samatov <maqsudsamadov719@gmail.com>
2026-08-06 17:13:25 +0500

    Allow transfer destination persistence

--- app/Models/AkademikMobillikAriza.php
+++ app/Models/AkademikMobillikAriza.php
@@ -14,6 +14,7 @@ class AkademikMobillikAriza extends Model
         'student_id',
         'phone',
         'reason',
+        'transfer_destination',
         'document_path',
         'document_name',
         'document_mime',

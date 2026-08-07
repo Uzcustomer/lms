af0efd77a3c271e0302d0e9461f942c6fa45d454
Maqsud Samatov <maqsudsamadov719@gmail.com>
2026-08-07 11:37:27 +0500

    Show academic mobility to admins

--- resources/views/components/admin-sidebar-menu.blade.php
+++ resources/views/components/admin-sidebar-menu.blade.php
@@ -1074,7 +1074,7 @@
         </a>
         @endif
 
-        @if($hasActiveRole('registrator_ofisi'))
+        @if($hasActiveRole(['superadmin', 'admin', 'registrator_ofisi']))
         <a href="{{ route('admin.academic-mobility.index') }}"
            class="sidebar-link {{ request()->routeIs('admin.academic-mobility.*') ? 'sidebar-active' : '' }}">
             <svg class="w-5 h-5 mr-3 sidebar-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">

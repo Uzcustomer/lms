d15775224a83943ae8275ede05a76e74498b6d34
Maqsud Samatov <maqsudsamadov719@gmail.com>
2026-08-10 09:47:12 +0500

    Add student transfer application model

---
+++ app/Models/StudentTransferApplication.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class StudentTransferApplication extends Model
+{
+    protected $fillable = [
+        'student_id',
+        'phone',
+        'reason',
+        'order_path',
+        'order_name',
+        'order_mime',
+        'order_size',
+        'status',
+    ];
+
+    public function student(): BelongsTo
+    {
+        return $this->belongsTo(Student::class);
+    }
+}
